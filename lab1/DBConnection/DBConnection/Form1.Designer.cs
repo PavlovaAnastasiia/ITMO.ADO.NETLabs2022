26e39c5e6e54d2f75cea32f271812cda360df1c4
Anastasiya Pavlova <AnastasiyaPavlova@itcenterifmo.onmicrosoft.com>
2022-02-22 22:53:43 +0300

    Add project files.

---
+++ lab1/DBConnection/DBConnection/Form1.Designer.cs
@@ -0,0 +1,94 @@
+﻿
+namespace DBConnection
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.Menu = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.MenuItemConnect = new System.Windows.Forms.ToolStripMenuItem();
+            this.MenuDisconnect = new System.Windows.Forms.ToolStripMenuItem();
+            this.Menu.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // Menu
+            // 
+            this.Menu.AccessibleRole = System.Windows.Forms.AccessibleRole.MenuItem;
+            this.Menu.AllowDrop = true;
+            this.Menu.AutoClose = false;
+            this.Menu.BackColor = System.Drawing.SystemColors.ControlDark;
+            this.Menu.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
+            this.Menu.DropShadowEnabled = false;
+            this.Menu.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.Menu.ImeMode = System.Windows.Forms.ImeMode.Katakana;
+            this.Menu.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.MenuItemConnect,
+            this.MenuDisconnect});
+            this.Menu.Name = "contextMenuStrip1";
+            this.Menu.RenderMode = System.Windows.Forms.ToolStripRenderMode.Professional;
+            this.Menu.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
+            this.Menu.ShowImageMargin = false;
+            this.Menu.Size = new System.Drawing.Size(187, 52);
+            this.Menu.TabStop = true;
+            this.Menu.Text = "Menu";
+            // 
+            // MenuItemConnect
+            // 
+            this.MenuItemConnect.Name = "MenuItemConnect";
+            this.MenuItemConnect.Size = new System.Drawing.Size(186, 24);
+            this.MenuItemConnect.Text = "Connect to DB";
+            this.MenuItemConnect.Click += new System.EventHandler(this.MenuItemConnect_Click);
+            // 
+            // MenuDisconnect
+            // 
+            this.MenuDisconnect.Name = "MenuDisconnect";
+            this.MenuDisconnect.Size = new System.Drawing.Size(186, 24);
+            this.MenuDisconnect.Text = "Disconnect from DB";
+            this.MenuDisconnect.Click += new System.EventHandler(this.MenuDisconnect_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ContextMenuStrip = this.Menu;
+            this.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.Name = "Form1";
+            this.Text = "lab1";
+            this.Menu.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private System.Windows.Forms.ContextMenuStrip Menu;
+        private System.Windows.Forms.ToolStripMenuItem MenuItemConnect;
+        private System.Windows.Forms.ToolStripMenuItem MenuDisconnect;
+    }
+}
+
---
+++ lab1/DBConnection/DBConnection/Form1.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.OleDb;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+
+namespace DBConnection
+{
+    public partial class Form1 : Form
+    {
+        OleDbConnection connection = new OleDbConnection();
+        string testConnect = @"Provider=SQLOLEDB.1;
+        Integrated Security=SSPI;Persist Security Info=False;
+        Initial Catalog=Northwind;Data Source=(local)";
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        
+
+        private void MenuItemConnect_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if (connection.State != ConnectionState.Open)
+                {
+                    connection.ConnectionString = testConnect;
+                    connection.Open();
+                    MessageBox.Show("Соединение с базой данных выполнено успешно");
+                }
+                else
+                    MessageBox.Show("Соединение с базой данных уже установлено");
+            }
+            //catch
+            //{
+            //    MessageBox.Show("Ошибка соединения с базой данных");
+            //}
+            catch (OleDbException XcpSQL)
+            {
+                foreach (OleDbError se in XcpSQL.Errors)
+                {
+                    MessageBox.Show(se.Message,
+                    "SQL Error code " + se.NativeError);
+                }
+            }
+            catch (Exception Xcp)
+            {
+                MessageBox.Show(Xcp.Message, "Unexpected Exception");
+            }
+        }
+
+        private void MenuDisconnect_Click(object sender, EventArgs e)
+        {
+            if (connection.State == ConnectionState.Open)
+            {
+                connection.Close();
+                MessageBox.Show("Соединение с базой данных закрыто");
+            }
+            else
+                MessageBox.Show("Соединение с базой данных уже закрыто");
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
